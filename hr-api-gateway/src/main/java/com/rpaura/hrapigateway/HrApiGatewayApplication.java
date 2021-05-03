2270ce70e58cc6b6decc306ed067f5e452066ba0
Rodrigo Paura <rpaura@indracompany.com>
2021-05-03 09:30:50 -0300

    Config Server

--- hr-api-gateway/src/main/java/com/rpaura/hrapigateway/HrApiGatewayApplication.java
+++ hr-api-gateway/src/main/java/com/rpaura/hrapigateway/HrApiGatewayApplication.java
@@ -2,9 +2,24 @@ package com.rpaura.hrapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("hr-worker", r -> r.path("/hr-worker/**")
+						.uri("lb://HR-WORKER"))
+				.route("hr-payroll", r -> r.path("/hr-payroll/**")
+						.uri("lb://HR-PAYROLL"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrApiGatewayApplication.class, args);
