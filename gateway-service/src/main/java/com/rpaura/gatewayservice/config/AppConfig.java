ca70b7854a1011b1a86e4c4d5d113b6d2a1342e6
Rodrigo Paura <rpaura@indracompany.com>
2021-05-05 14:22:25 -0300

    create auth

---
+++ gateway-service/src/main/java/com/rpaura/gatewayservice/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.rpaura.gatewayservice.config;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+public class AppConfig {
+
+}
